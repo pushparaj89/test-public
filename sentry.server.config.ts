1509cbc25f9aaf3c1bc52e77cda5b6360be447ee
jesse.vallbadass <jesse.vallbadass@nhvr.gov.au>
2024-11-01 13:04:38 +1000

    fix to add packages and sentry base

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from '@sentry/nextjs';
+
+Sentry.init({
+  dsn: 'https://c998a99436c03d9d9d2fe44aea19a170@o4508218111033344.ingest.us.sentry.io/4508218135150592',
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
